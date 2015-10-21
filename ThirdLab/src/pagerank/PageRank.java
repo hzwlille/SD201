3c482e8627e7274e6578fbdcba355b2655db1db4
hzwlille <kxyh@qq.com>
2015-10-21 19:08:37 +0200

    continue

--- ThirdLab/src/pagerank/PageRank.java
+++ ThirdLab/src/pagerank/PageRank.java
@@ -1,13 +1,18 @@
 package pagerank;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
 
 /*
  * VERY IMPORTANT 
@@ -42,16 +47,42 @@ public class PageRank {
 		//TO DO			
 	}
 	
-	public static boolean checkConvergence(String initialDirPath, String iterationDirPath, double epsilon)
+	public static boolean checkConvergence(String initialDirPath, String iterationDirPath, double epsilon) throws NumberFormatException, IOException
 	{
 		//TO DO
 		// you need to use the L1 norm 
-		return false;
+		
+		//Récupérer les vecteurs
+		BufferedReader iniVect = new BufferedReader(new FileReader(initialDirPath + "/vector.txt"));
+		BufferedReader iteVect = new BufferedReader(new FileReader(iterationDirPath + "/vector.txt"));
+		double ecart=0;
+		
+		while(iniVect.hashCode()==1){//?
+			
+			ecart=ecart+Math.abs(Double.parseDouble(iniVect.readLine())-Double.parseDouble(iteVect.readLine()));
+			
+		}
+		
+		if(ecart<epsilon){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
 	}
 	
-	public static void avoidSpiderTraps(String vectorDirPath, long nNodes, double beta) 
+	public static void avoidSpiderTraps(String vectorDirPath, long nNodes, double beta) throws IOException 
 	{
 		//TO DO
+		BufferedWriter output = new BufferedWriter(new FileWriter(vectorDirPath + "/vector.txt"));
+		double[nNodes] vectorX;
+		for(int i=0;i<n;i++){
+			output.write(String.format("%.5f", a));
+			output.write("\n");
+		}
+		
+		
 		
 	}
 	
@@ -69,14 +100,37 @@ public class PageRank {
 		Double epsilon = conf.getDouble("epsilon", 0.1);
 		Double beta = conf.getDouble("beta", 0.8);
 
- 
+		boolean getResult=false;
 		//TO DO
 
 		// to retrieve the number of nodes use long nNodes = conf.getLong("numNodes", 0); 
 		Long nNodes = conf.getLong("numNodes", 0); 
-		createInitialRankVector(initialVector,nNodes);
 		
-
+		
+		//Creat the initial RankVector 
+		createInitialRankVector(initialVector,nNodes);
+		File dir = new File(currentVector);
+		
+		
+		//Get the matrix
+		GraphToMatrix.job(conf);
+		
+		
+		while(!getResult){
+		//Calculer le nouveau vecteur
+		avoidSpiderTraps(initialVector,nNodes,beta);
+		
+		//Comparer l'ecart
+		getResult=checkConvergence(initialVector,currentVector,epsilon);
+		if(!getResult){
+												//copy current to initial.
+		}
+			
+		}
+		
+													//copy current to result
+		
+		
 		// when you finished implementing delete this line
 		throw new UnsupportedOperationException("Implementation missing");
 		
