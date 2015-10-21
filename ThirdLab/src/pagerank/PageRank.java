f6f575f8e066553baa1658c5fa1769b2dbe40a37
hzwlille <kxyh@qq.com>
2015-10-21 22:56:07 +0200

    haha

--- ThirdLab/src/pagerank/PageRank.java
+++ ThirdLab/src/pagerank/PageRank.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -36,9 +37,12 @@ public class PageRank {
 		if(! dir.exists())
 			FileUtils.forceMkdir(dir);
 		
-		BufferedWriter output = new BufferedWriter(new FileWriter(directoryPath + "/vector.txt"));
+		BufferedWriter output = new BufferedWriter(new FileWriter(directoryPath + "/part-r-00000"));
 		float a= (float)1/n;
 		for(int i=0;i<n;i++){
+
+		output.write(String.valueOf(i+1));
+		output.write("\t");
 		output.write(String.format("%.5f", a));
 		output.write("\n");
 		}
@@ -53,16 +57,25 @@ public class PageRank {
 		// you need to use the L1 norm 
 		
 		//Récupérer les vecteurs
-		BufferedReader iniVect = new BufferedReader(new FileReader(initialDirPath + "/vector.txt"));
-		BufferedReader iteVect = new BufferedReader(new FileReader(iterationDirPath + "/vector.txt"));
-		double ecart=0;
 		
-		while(iniVect.hashCode()==1){//?
-			
-			ecart=ecart+Math.abs(Double.parseDouble(iniVect.readLine())-Double.parseDouble(iteVect.readLine()));
+		BufferedReader iniVect = new BufferedReader(new FileReader(initialDirPath + "/part-r-00000"));
+		BufferedReader iteVect = new BufferedReader(new FileReader(iterationDirPath + "/part-r-00000"));
+		
+		double ecart=0;
+		String lineG;
+		String lineD;
+		
+		for(int i=0;i<7;i++){//?
+
+			lineG=iniVect.readLine();
+			lineD=iteVect.readLine();
+			ecart=ecart+Math.abs(Double.parseDouble(lineG.split("\t")[1])-Double.parseDouble(lineD.split("\t")[1]));
+
 			
 		}
-		
+
+		iniVect.close();
+		iteVect.close();
 		if(ecart<epsilon){
 			return true;
 		}
@@ -75,15 +88,33 @@ public class PageRank {
 	public static void avoidSpiderTraps(String vectorDirPath, long nNodes, double beta) throws IOException 
 	{
 		//TO DO
-		BufferedWriter output = new BufferedWriter(new FileWriter(vectorDirPath + "/vector.txt"));
-		double[nNodes] vectorX;
-		for(int i=0;i<n;i++){
-			output.write(String.format("%.5f", a));
-			output.write("\n");
+		BufferedReader output = new BufferedReader(new FileReader(vectorDirPath + "/part-r-00000"));
+		double use;
+		String line;
+		ArrayList<Double> newVector=new ArrayList<Double>();
+		for(int i=0;i<nNodes;i++){
+			line=output.readLine();
+			System.out.println();
+			System.out.println(line.split("\t")[1]);
+			use=Double.parseDouble(line.split("\t")[1]);
+			use=use*beta+((1-beta)/nNodes);
+			newVector.add(use);
 		}
+		System.out.println("I arrive here haha3");
 		
+
+		output.close();
+		BufferedWriter input = new BufferedWriter(new FileWriter(vectorDirPath + "/part-r-00000"));
+		for(int i=0;i<nNodes;i++){
+			input.write(String.valueOf(i+1));
+			input.write("\t");
+			input.write((String.format("%.5f", newVector.get(i))));
+			input.write("\n");
+
+		}
+		input.close();
 		
-		
+
 	}
 	
 	public static void iterativePageRank(Configuration conf) 
@@ -109,30 +140,60 @@ public class PageRank {
 		
 		//Creat the initial RankVector 
 		createInitialRankVector(initialVector,nNodes);
-		File dir = new File(currentVector);
 		
 		
-		//Get the matrix
+		//Get the matrix and copie to inputMatrixPath
 		GraphToMatrix.job(conf);
-		
-		
+
+
+
 		while(!getResult){
-		//Calculer le nouveau vecteur
-		avoidSpiderTraps(initialVector,nNodes,beta);
-		
-		//Comparer l'ecart
-		getResult=checkConvergence(initialVector,currentVector,epsilon);
-		if(!getResult){
-												//copy current to initial.
-		}
+
+			System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
+			//Calculer le nouveau vecteur
+			
+			MatrixVectorMult.job(conf);
+
+			System.out.println("++++++++++++++++++++");
+			avoidSpiderTraps(currentVector,nNodes,beta);
+			//Comparer l'ecart
+			getResult=checkConvergence(initialVector,currentVector,epsilon);
+			System.out.println("The get result is :");
+			System.out.print(getResult);
+			if(!getResult){
+				
+				
+				BufferedReader curVect = new BufferedReader(new FileReader(currentVector + "/part-r-00000"));
+				BufferedWriter iteVect = new BufferedWriter(new FileWriter(initialVector + "/part-r-00000"));
+				for(int i=0;i<nNodes;i++){
+				String myStr;
+				myStr=curVect.readLine();
+				System.out.println(myStr);
+					iteVect.write(myStr);
+					iteVect.write("\n");
+					
+
+				}
+				iteVect.close();
+				curVect.close();
+				FileUtils.deleteDirectory(new File(currentVector));
+				
+				/*FileUtils.deleteDirectory(new File(initialVector));
+				FileUtils.copyDirectory(new File(currentVector), new File(initialVector));								//copy current to initial.		
+				
+				*/
+			}
+			System.out.println("One turn finished");
 			
 		}
-		
+
 													//copy current to result
 		
-		
+		//FileUtils.deleteDirectory(new File(conf.get("inputMatrixPath")));
+		//FileUtils.deleteDirectory(new File(conf.get(currentVector)));
 		// when you finished implementing delete this line
-		throw new UnsupportedOperationException("Implementation missing");
+		FileUtils.copyDirectory(new File(initialVector), new File(conf.get("finalVectorPath")));
+		
 		
 	}
 }
